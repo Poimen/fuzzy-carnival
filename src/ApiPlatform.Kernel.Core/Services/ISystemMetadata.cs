263bf960cd2473ac70d761a22bfd5dcd73c0c334
Richard Shephard <richard.shephard@gmail.com>
2023-12-02 11:21:29 +0200

    add initial staging of project

---
+++ src/ApiPlatform.Kernel.Core/Services/ISystemMetadata.cs
@@ -0,0 +1,9 @@
+﻿
+namespace ApiPlatform.Kernel.Core.Services
+{
+    public interface ISystemMetadata
+    {
+        CancellationToken CancellationToken { get; }
+        Dictionary<string, string> Metadata { get; }
+    }
+}
---
+++ src/ApiPlatform.Kernel.Core/Services/SystemMetadata.cs
@@ -0,0 +1,25 @@
+﻿namespace ApiPlatform.Kernel.Core.Services
+{
+    public class SystemMetadata : ISystemMetadata
+    {
+        public CancellationToken CancellationToken { get; }
+
+        public Dictionary<string, string> Metadata { get; }
+
+        private SystemMetadata(CancellationToken cancellationToken)
+        {
+            CancellationToken = default;
+            Metadata = [];
+        }
+
+        public void Add(string key, string value)
+        {
+            Metadata.TryAdd(key, value);
+        }
+
+        public static ISystemMetadata Create(CancellationToken cancellationToken = default)
+        {
+            return new SystemMetadata(cancellationToken);
+        }
+    }
+}
